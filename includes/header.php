883500d06928fa8510cd6e53f0e9bb2039ab9457
shivam24121 <Shivam050905@gmail.com>
2025-06-27 21:24:55 +0530

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hostel Booking System</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
